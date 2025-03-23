69815d1f25671d3ad9b8efad630cae75e8587c78
Jacob Nef <jacob@nef-family.net>
2025-03-23 16:07:30 -0600

    implement logout for SignedInClient

--- client/src/main/java/ui/SignedInClient.java
+++ client/src/main/java/ui/SignedInClient.java
@@ -1,6 +1,10 @@
 package ui;
 
+import exception.ResponseException;
+import model.request.LogoutRequest;
 import server.ServerFacade;
+
+import java.util.Arrays;
 
 public class SignedInClient implements ClientInterface{
     private final Repl repl;
@@ -11,7 +15,40 @@ public class SignedInClient implements ClientInterface{
 
     @Override
     public String eval(String input) {
-        return input;
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch(cmd) {
+                case "create" -> create(params);
+                case "logout" -> logout();
+                case "list" -> list();
+                case "play" -> play(params);
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (ResponseException e){
+            return e.getMessage();
+        }
+    }
+
+    public String create(String... params){
+        return null;
+    }
+
+    public String logout() throws ResponseException {
+        repl.getServer().logout(new LogoutRequest(repl.getAuthToken()));
+        repl.setAuthToken(null);
+        repl.setState(State.SIGNED_OUT);
+        return "Successfully logged out.";
+    }
+
+    public String list(){
+        return null;
+    }
+
+    public String play(String... params){
+        return null;
     }
 
     @Override
